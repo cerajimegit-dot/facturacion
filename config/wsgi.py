bab50dc98e237f89455e97616ad944bcb4e52b72
Facturacion Dev <dev@facturacion.local>
2026-04-17 11:39:54 -0300

    Fix Vercel: apuntar build a wsgi.py en vez de manage.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,3 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
+
+# Vercel busca una variable llamada 'app'
+app = application
